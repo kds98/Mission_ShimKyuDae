dc4b9dd8ce0598103eba0ff97db29a853f4b6144
kds98 <skyudae@naver.com>
2023-11-06 11:20:01 +0900

    1단계 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,7 +1,20 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class App {
     public static void run(){
+        String cmd;
+
+        System.out.println("== 명언 앱 ==");
+        System.out.print("명령) ");
+
+        Scanner scanner = new Scanner(System.in);
+        cmd = scanner.nextLine();
+
+        if(cmd == "종료"){
+            System.exit(0);
+        }
 
     }
 }
---
+++ src/test/java/com/ll/AppTest.java
@@ -0,0 +1,11 @@
+package com.ll;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+}
