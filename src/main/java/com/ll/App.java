5aebd6da8e22b312dcf8c1807449cf756c80c71d
kds98 <skyudae@naver.com>
2023-11-06 11:46:05 +0900

    3단계 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,21 +3,25 @@ package com.ll;
 import java.util.Scanner;
 
 public class App {
+    int quotesNumber = 0;
     Scanner scanner = new Scanner(System.in);
+
     public void run() {
         String cmd;
 
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
+        while (true) {
+            System.out.print("명령) ");
 
-        cmd = scanner.nextLine();
+            cmd = scanner.nextLine();
 
-        if (cmd.equals("종료")) {
-            System.exit(0);
-        }
+            if (cmd.equals("종료")) {
+                System.exit(0);
+            }
 
-        if (cmd.equals("등록")) {
-            createQuotes();
+            if (cmd.equals("등록")) {
+                createQuotes();
+            }
         }
     }
 
@@ -26,5 +30,7 @@ public class App {
         String quotes = scanner.nextLine();
         System.out.print("작가 : ");
         String author = scanner.nextLine();
+        quotesNumber++;
+        System.out.printf("%d번 명언이 등록되었습니다.\n", quotesNumber);
     }
 }
