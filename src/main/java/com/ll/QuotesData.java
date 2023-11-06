29166eeff8314f4ff07b9b3acef3d33d94bae529
kds98 <skyudae@naver.com>
2023-11-06 17:42:42 +0900

    QuotesData 클래스 추가

---
+++ src/main/java/com/ll/QuotesData.java
@@ -0,0 +1,31 @@
+package com.ll;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+public class QuotesData {
+    private int index;
+    private String autorName;
+    private String quote;
+
+    public QuotesData(int index, String autorName, String quote){
+        this.index = index;
+        this.autorName = autorName;
+        this.quote = quote;
+    }
+
+    public int getIndex(){
+        return index;
+    }
+
+    public String getAutorName(){
+        return autorName;
+    }
+
+    public String getQuote(){
+        return quote;
+    }
+
+
+}
