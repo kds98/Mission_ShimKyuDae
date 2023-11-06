46093a25501b27fc9b82ad693d5a0d70c5c1e0dd
kds98 <skyudae@naver.com>
2023-11-06 11:39:30 +0900

    2단계 구현

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,18 +3,28 @@ package com.ll;
 import java.util.Scanner;
 
 public class App {
-    public static void run(){
+    Scanner scanner = new Scanner(System.in);
+    public void run() {
         String cmd;
 
         System.out.println("== 명언 앱 ==");
         System.out.print("명령) ");
 
-        Scanner scanner = new Scanner(System.in);
         cmd = scanner.nextLine();
 
-        if(cmd == "종료"){
+        if (cmd.equals("종료")) {
             System.exit(0);
         }
 
+        if (cmd.equals("등록")) {
+            createQuotes();
+        }
+    }
+
+    private void createQuotes() {
+        System.out.print("명언 : ");
+        String quotes = scanner.nextLine();
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
     }
 }
